b7d00bae0e26e86a31a9e658987d82b88dec74ba
Christopher <christopher.r.elliott20.mil@army.mil>
2025-06-17 23:23:57 -0400

    another case update

---
+++ chatroomserver/gen_certs_run.sh
@@ -0,0 +1,5 @@
+openssl genpkey -algorithm RSA -out server.key
+openssl req -new -key server.key -out server.csr
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+chmod +x chat_room
+./chat_room
